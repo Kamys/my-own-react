23944b3c662f6f102e47c912a4fc36969fe31e0f
Nikita Kamyshenko <nikita.kamyshenko@mail.ru>
2019-08-16 09:17:11 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+interface IVDOM {
+    type: string;
+    props: {
+        children?: IVDOM[] | string;
+        [key: string]: any
+    }
+}
+
+const onChange = (event) => {
+    // @ts-ignore
+    console.log('event: ', event, this);
+};
+
+const element: IVDOM = {
+    type: 'div',
+    props: {
+        id: 'container',
+        children: [
+            {
+                type: 'div', props: {
+                    children: [
+                        {type: 'input', props: {value: 'foo', type: 'text', onChange}},
+                    ]
+                }
+            },
+            {type: 'a', props: {href: '/bar', children: 'Click me'}},
+            {type: 'span', props: {}}
+        ]
+    }
+};
+
+console.log('Hello v1');
+
+const root = document.getElementById('root');
+
+const isString = (value: any): value is string => {
+    return typeof value === 'string' || value instanceof String
+};
+
+const isListner = (propsKey: string): boolean => {
+    return propsKey.startsWith('on');
+};
+
+const render = (element: IVDOM, parent) => {
+    const newElement = document.createElement(element.type);
+    const {children, ...otherProps} = element.props;
+    Object.entries(otherProps).forEach(([key, value]) => {
+        if (isListner(key)) {
+            newElement.addEventListener(key, value)
+        } else {
+            newElement[key] = value;
+        }
+    });
+    if (children) {
+        if (isString(children)) {
+            const textNode = document.createTextNode(children);
+            newElement.appendChild(textNode);
+        } else {
+            children.forEach((child) => render(child, newElement))
+        }
+    }
+    parent.appendChild(newElement);
+};
+
+render(element, root);
